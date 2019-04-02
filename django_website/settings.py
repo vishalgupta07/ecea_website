a77d69a8b8ea31eb3b4e1c7f6075b3448cb4e43a
vishal862 <vishalgupta862@gmail.com>
2019-04-02 19:02:07 +0530

    pass commit

--- django_website/settings.py
+++ django_website/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$z8vx&@sts(he$8a7+tz*_4h37w9(ki#=!3+3q9=vb%mzds%l0'
-
+# SECRET_KEY = '$z8vx&@sts(he$8a7+tz*_4h37w9(ki#=!3+3q9=vb%mzds%l0'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['eceadjangowebsite.herokuapp.com', 'localhost']
 
 
 # Application definition
